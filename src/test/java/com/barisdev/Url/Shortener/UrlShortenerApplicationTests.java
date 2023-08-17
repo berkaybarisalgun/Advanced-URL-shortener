10d04867811754d3586594ffec862336754afae6
Berkay Baris Algun <bbarisalgun@gmail.com>
2023-08-17 15:39:15 +0300

    added required dependencies

---
+++ src/test/java/com/barisdev/Url/Shortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barisdev.Url.Shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
