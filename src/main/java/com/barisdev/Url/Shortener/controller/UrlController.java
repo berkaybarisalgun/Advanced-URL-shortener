6b50ff54ad66df490ea71bc73d764846701161ca
Berkay Baris Algun <bbarisalgun@gmail.com>
2023-08-22 19:29:24 +0300

    Added post controller
    Added post controller and in next

--- src/main/java/com/barisdev/Url/Shortener/controller/UrlController.java
+++ src/main/java/com/barisdev/Url/Shortener/controller/UrlController.java
@@ -3,6 +3,7 @@ package com.barisdev.Url.Shortener.controller;
 import com.barisdev.Url.Shortener.dto.DtoToEntityConverter;
 import com.barisdev.Url.Shortener.dto.UrlDto;
 import com.barisdev.Url.Shortener.entity.Url;
+import com.barisdev.Url.Shortener.request.UrlRequest;
 import com.barisdev.Url.Shortener.request.UrlRequestToUrlConverter;
 import com.barisdev.Url.Shortener.service.UrlService;
 import jakarta.validation.Valid;
@@ -10,10 +11,7 @@ import jakarta.validation.constraints.NotEmpty;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -27,13 +25,14 @@ public class UrlController {
 
     private final DtoToEntityConverter dtoToEntityConverter;
 
+    private final UrlRequestToUrlConverter urlRequestToUrlConverter;
+
     public UrlController(UrlService service, DtoToEntityConverter dtoToEntityConverter, UrlRequestToUrlConverter urlRequestToUrlConverter) {
         this.service = service;
         this.dtoToEntityConverter = dtoToEntityConverter;
         this.urlRequestToUrlConverter = urlRequestToUrlConverter;
     }
 
-    private final UrlRequestToUrlConverter urlRequestToUrlConverter;
 
 
     @GetMapping
@@ -58,5 +57,31 @@ public class UrlController {
         );
 
     }
+
+    @GetMapping("/{code}")
+    public ResponseEntity<UrlDto> redirect(@Valid @NotEmpty @PathVariable String code) throws URISyntaxException {
+
+        Url shortUrl = service.getUrlByref(code);
+
+        URI uri = new URI(shortUrl.getUrl());
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(uri);
+
+        return new ResponseEntity<UrlDto>(
+                httpHeaders, HttpStatus.SEE_OTHER
+        );
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody UrlRequest shortUrlRequest) {
+        Url shortUrl = urlRequestToUrlConverter.urlRequestToUrl(shortUrlRequest);
+        return new ResponseEntity<UrlDto>(
+                dtoToEntityConverter.convertToDto(service.create(shortUrl)), HttpStatus.CREATED
+        );
+    }
+
+
+
+
 
 }
