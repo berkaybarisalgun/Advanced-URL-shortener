9c1e9f0a600ab268c91e32873d658d0d2e1b63fd
Berkay Baris Algun <bbarisalgun@gmail.com>
2023-08-24 10:24:53 +0300

    Random String generation has changed.

--- src/main/java/com/barisdev/Url/Shortener/util/RandomStringGenerator.java
+++ src/main/java/com/barisdev/Url/Shortener/util/RandomStringGenerator.java
@@ -1,35 +1,29 @@
 package com.barisdev.Url.Shortener.util;
 
-import org.springframework.stereotype.Component;
+import java.security.SecureRandom;
 
-import java.util.Collections;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.Random;
-import java.util.stream.Collectors;
 
-@Component
 public class RandomStringGenerator {
+    private final Random random;
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNPQRSTUVWXYZ123456789";
 
-    private int refLen=5;
-
-    public String generateRandomString(){
-        Random random=new Random();
-
-        String generated="";
-
-        var letters="abcdefghijklmnprstyzwx123456789"
-                .toUpperCase()
-                .chars()
-                .mapToObj(x->(char)x)
-                .collect(Collectors.toList());
-
-        Collections.shuffle(letters);
-
-        for(int i=0;i<refLen;i++){
-            generated+=letters.get(random.nextInt(letters.size()));
-        }
-        return generated;
-
+    public RandomStringGenerator() {
+        this.random = new SecureRandom();
     }
 
+    public String generateRandomString() {
+        if (5 <= 0) {
+            throw new IllegalArgumentException("Length must be greater than 0");
+        }
+
+        StringBuilder sb = new StringBuilder(5);
+        for (int i = 0; i < 5; i++) {
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
 
 }
